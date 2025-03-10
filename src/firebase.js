1dde30dbb08f89fde3e99c78ff821f279ea40dc3
Achyut Singh <111772817+Rookie768@users.noreply.github.com>
2025-03-10 13:50:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -24,23 +24,23 @@ import {
   orderBy 
 } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCnbkRrfXAzoiXyEBHx6d90lVqK9Gdto2E",
-//   authDomain: "todo-list-app-bd60a.firebaseapp.com",
-//   projectId: "todo-list-app-bd60a",
-//   storageBucket: "todo-list-app-bd60a.appspot.com",
-//   messagingSenderId: "41631197069",
-//   appId: "1:41631197069:web:832f16e6aed85f7f2651c4"
-// };
-
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCnbkRrfXAzoiXyEBHx6d90lVqK9Gdto2E",
+  authDomain: "todo-list-app-bd60a.firebaseapp.com",
+  projectId: "todo-list-app-bd60a",
+  storageBucket: "todo-list-app-bd60a.appspot.com",
+  messagingSenderId: "41631197069",
+  appId: "1:41631197069:web:832f16e6aed85f7f2651c4"
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
